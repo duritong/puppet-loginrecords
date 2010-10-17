98127806e639643cb6b8f3fcf8243a14bba248ce
intrigeri <intrigeri@boum.org>
2010-10-17 18:41:19 +0200

    Fix default settings.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,10 @@
 class loginrecords {
 
+    # Default settings
+    if $enable_faillog == '' {
+        $enable_faillog = false
+    }
+
     case $kernel {
         "Linux": {
             case  $operatingsystem {
