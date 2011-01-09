5b303f63c2133e25f84b21f7c2b275aa02c32545
mh <mh@immerda.ch>
2011-01-09 22:51:26 +0100

    Disable removing utmp by default
    This breaks various scripts such as runlevel and hence invoke-rc.d

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,7 +2,7 @@ class loginrecords(
     $disable_btmp = true,
     $disable_faillog = true,
     $disable_lastlog = true,
-    $disable_utmp = true,
+    $disable_utmp = false,
     $disable_wtmp = true
 ){
     # Include main class
