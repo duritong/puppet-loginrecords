b8446fb41516f5cc61e8d230802191e891031996
mh <mh@immerda.ch>
2011-03-04 17:38:14 +0100

    fixing override typo

--- manifests/ramrun/disable.pp
+++ manifests/ramrun/disable.pp
@@ -1,6 +1,6 @@
 class loginrecords::ramrun::disable inherits loginrecords::ramrun::enable {
 
-  Augeas { "ramdisk-on-var-run":
+  Augeas["ramdisk-on-var-run"]{
     changes => "set RAMRUN yes",
   }
 
