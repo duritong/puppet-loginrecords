868c2bee3b8c77a01bdd2b003c0560c3006a0f8f
mh <mh@immerda.ch>
2012-06-05 20:06:17 -0300

    new style for 2.7

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,22 +1,21 @@
 class loginrecords(
-    $disable_btmp = true,
-    $disable_faillog = true,
-    $disable_lastlog = true,
-    $protect_utmp = true,
-    $disable_wtmp = true,
-    $ramdisk_on_var_run = true
+  $disable_btmp = true,
+  $disable_faillog = true,
+  $disable_lastlog = true,
+  $protect_utmp = true,
+  $disable_wtmp = true,
+  $ramdisk_on_var_run = true
 ){
-    # Include main class
-    case $kernel {
-        "Linux": {
-            case  $operatingsystem {
-                "debian", "ubuntu": { include loginrecords::debian }
-                default:            { include loginrecords::base   }
-            }
-        }
-        default: {
-            err("Kernel $kernel is not supported.")
-        }
+  # Include main class
+  case $::kernel {
+    "Linux": {
+      case  $::operatingsystem {
+        "debian", "ubuntu": { include loginrecords::debian }
+        default:            { include loginrecords::base   }
+      }
     }
-
+    default: {
+      err("Kernel ${::kernel} is not supported.")
+    }
+  }
 }
