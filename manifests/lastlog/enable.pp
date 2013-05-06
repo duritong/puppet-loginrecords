737079e7e0681a09b8a3cc1bbdd2a82bfd01609b
intrigeri <intrigeri@boum.org>
2013-05-06 14:59:23 +0200

    Linting.

--- manifests/lastlog/enable.pp
+++ manifests/lastlog/enable.pp
@@ -2,15 +2,15 @@
 class loginrecords::lastlog::enable(
   $pam_login_file = '/etc/pam.d/login'
 ){
-  file_line{'loginrecords-lastlog':
-    path        => $pam_login_file,
-    match       => '^.*session[[:space:]]+optional[[:space:]]+pam_lastlog.so$',
-    line => 'session  optional   pam_lastlog.so',
+  file_line{ 'loginrecords-lastlog':
+    path  => $pam_login_file,
+    match => '^.*session[[:space:]]+optional[[:space:]]+pam_lastlog.so$',
+    line  => 'session  optional   pam_lastlog.so',
   }
-  file{'/var/log/lastlog':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'utmp',
-    mode    => '0664';
+  file{ '/var/log/lastlog':
+    ensure => present,
+    owner  => root,
+    group  => utmp,
+    mode   => '0664';
   }
 }
